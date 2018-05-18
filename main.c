31c03f9d2e3091db6ae54f76debee654f93205dc
kscg5168 <gorden88115@gmail.com>
2018-05-18 10:06:27 +0800

    first commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a=2,b=3,c;
+	c=a+b;
+    printf ("%d+%d=%d",a,b,c); 
+	return 0;
+}
