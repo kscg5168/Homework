875dd5c5fa26942bff81afaf771d80e4587b9e8f
kscg5168 <gorden88115@gmail.com>
2018-05-18 10:10:24 +0800

    camel case

--- main.c
+++ main.c
@@ -4,8 +4,8 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int a=2,b=3,c;
-	c=a+b;
-    printf ("%d+%d=%d",a,b,c); 
+	int two=2,three=3,sum;
+	sum=two+three;
+    printf ("%d+%d=%d",two,three,sum); 
 	return 0;
 }
